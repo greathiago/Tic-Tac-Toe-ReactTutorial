e5399efe655cb06c85a466b29f59a80705955d12
greathiago <greathiago@users.noreply.github.com>
2019-05-06 14:16:40 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -79,17 +79,13 @@ function Square(props)
       return (
         <div>
           <div className="status">{status}</div>
-          <div className="board-row">
+          <div className="board-grid">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
             {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
             {this.renderSquare(3)}
             {this.renderSquare(4)}
             {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
             {this.renderSquare(6)}
             {this.renderSquare(7)}
             {this.renderSquare(8)}
